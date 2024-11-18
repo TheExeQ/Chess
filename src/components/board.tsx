fced8fd02ec602f94a281b952deca1985a993b2c
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-11-18 21:53:28 +0100

    feat: added functionality to only allow legal moves

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,12 +1,43 @@
 ﻿"use client";
 
-// Bare Minimum
+import { useState } from "react";
+import { Chess, Square } from "chess.js";
 import { Chessboard } from "react-chessboard";
 
+type MoveInput = { from: string; to: string; promotion?: string };
+
 export default function Board() {
+    const [game, setGame] = useState<Chess>(new Chess());
+
+    function makeAMove(move: MoveInput){
+        let result;
+        try {
+            const gameCopy = new Chess(game.fen());
+            result = gameCopy.move(move);
+            if (result) {
+                setGame(gameCopy);
+            }
+            
+        } catch (e) {
+            console.log(e);
+        }
+        return result;
+    }
+    
+    function onDrop(sourceSquare: Square, targetSquare: Square) {
+        const move = makeAMove({
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: "q",
+        });
+
+        if (move === null) return false;
+        return true;
+    }
+    
     return (
         <div>
-            <Chessboard id="BasicBoard" />
+            <Chessboard position={game.fen()} onPieceDrop={onDrop} />
         </div>
     );
 }
